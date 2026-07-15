5f3c6e66c7ae373537c195c990ca4d469cf94041
kailera <ferreirasamuel459@gmail.com>
2026-07-15 15:48:32 -0300

    update: docker entry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,7 @@ echo "[INFO] Verificando se o schema do Prisma existe no container:"
 ls -la prisma/schema.prisma || echo "[ERRO] ❌ O schema.prisma não foi encontrado no container!"
 
 echo "[INFO] Executando migrations do Prisma no banco de dados..."
-npx prisma migrate deploy --schema=./prisma/schema.prisma
+npx prisma migrate deploy
 
 echo "[INFO] 🚀 Iniciando aplicação: $@"
 exec "$@"
